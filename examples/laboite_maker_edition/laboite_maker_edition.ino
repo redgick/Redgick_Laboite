c32d3c34add5fdfc917d81dcec66dff3a7d3163a
Julien Lebunetel <julien@lebunetel.com>
2018-05-25 13:02:12 +0200

    Change url lenuage.redgick.com to lenuage.io

--- examples/laboite_maker_edition/laboite_maker_edition.ino
+++ examples/laboite_maker_edition/laboite_maker_edition.ino
@@ -1,6 +1,6 @@
 /*
    Firmware for the connected display "laboite maker" by Redgick.
-   Create an account on "https://lenuage.redgick.com" and get an api key.
+   Create an account on "https://lenuage.io" and get an api key.
 */
 
 // Two modules MAX7219 4 in 1 connected in serial to compose a monochrome 32x16 LED matrix
@@ -19,11 +19,11 @@ Screen welcome(32, 16, MONOCHROME); // with, height, colors);
 
 // Backend connection
 Lenuage lenuage(
-  "http://lenuage.redgick.com",             // server url
+  "http://lenuage.io",                      // server url
   "00000000-0000-0000-0000-000000000000",   // api key
   // the server ssl fingerprint is needed for a HTTPS connection
   // get it using the following command in a terminal:
-  // echo | openssl s_client -connect redgick.com:443 |& openssl x509 -fingerprint -noout
+  // echo | openssl s_client -connect lenuage.io:443 |& openssl x509 -fingerprint -noout
   "AD:4D:92:32:D3:D2:5A:C4:5B:62:B6:B4:94:BF:10:94:4F:71:A5:5A"
 );
 
